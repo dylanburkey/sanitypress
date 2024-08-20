1b531c4abf2d1e32be01b3ea59c51fdb56d43e67
Dylan Burkey <dylan.burkey@zebraskimmers.com>
2024-08-20 16:24:27 -0400

    [short summary of changes]
    [Describe previous functionality and its issues]
    [Describe current functionality]
    [Describe reasoning behind the chosen solution]

--- next.config.js
+++ next.config.js
@@ -27,11 +27,11 @@ const nextConfig = {
 		}`)
 	},
 
-	// logging: {
-	// 	fetches: {
-	// 		fullUrl: true,
-	// 	},
-	// },
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
 }
 
 module.exports = nextConfig
